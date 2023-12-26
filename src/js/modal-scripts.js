b533973550077694a5385f573546c478e5d09e9e
Dmytro Zl <zl.dmitriy94@gmail.com>
2023-12-26 11:51:28 +0200

    Update modal-scripts.js

--- src/js/modal-scripts.js
+++ src/js/modal-scripts.js
@@ -5,10 +5,11 @@ function openModal() {
     document.getElementById("mobile-menu").className = "mobile-menu-wrapper is-open";
 }
 
-const modalCloser = document.getElementsByClassName("modal-close");
-for (let i = 0; i < modalCloser.length; i++) {
-    modalCloser[i].addEventListener('click', closeModal, false);
+const modalClose = document.getElementsByClassName("modal-close");
+for (let i = 0; i < modalClose.length; i++) {
+    modalClose[i].addEventListener('click', closeModal, false);
 }
 function closeModal() {
     document.getElementById("mobile-menu").className = "mobile-menu-wrapper";
 }
+
